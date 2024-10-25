7ab2adf4acd8a63e356b7701421ee58bc993b8b5
Lanvizu <ghzm888@gmail.com>
2024-10-25 16:51:44 +0900

    Initial commit

---
+++ src/main/java/com/study/NIHo/NiHoApplication.java
@@ -0,0 +1,13 @@
+package com.study.NIHo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiHoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiHoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/NIHo/NiHoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.NIHo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiHoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
