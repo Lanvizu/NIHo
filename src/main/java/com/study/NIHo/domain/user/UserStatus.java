001a80428a58e6a895d62eb3bdeddd7d3cac4fe0
Lanvizu <ghzm888@gmail.com>
2024-12-02 17:15:44 +0900

    feat: 유저 상태에 관한 Enum 타입 UserStatus 생성

---
+++ src/main/java/com/study/NIHo/domain/user/UserStatus.java
@@ -0,0 +1,17 @@
+package com.study.NIHo.domain.user;
+
+import lombok.Getter;
+
+@Getter
+public enum UserStatus {
+    BAN("BAN", "밴 처리된 회원"),
+    NORMAL("NORMAL", "회원 상태 양호");
+
+    private final String code;
+    private final String statusInfo;
+
+    UserStatus(String code, String statusInfo) {
+        this.code = code;
+        this.statusInfo = statusInfo;
+    }
+}
