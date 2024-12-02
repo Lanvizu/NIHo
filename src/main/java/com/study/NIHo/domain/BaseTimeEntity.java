789c1c25f9713b40670f5ae159c15a45f9312477
Lanvizu <ghzm888@gmail.com>
2024-12-02 17:14:42 +0900

    feat: 생성, 수정 시간 관련 BaseTimeEntity 생성

---
+++ src/main/java/com/study/NIHo/domain/BaseTimeEntity.java
@@ -0,0 +1,38 @@
+package com.study.NIHo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    private String createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private String updatedDate;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        this.updatedDate = this.createdDate;
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        this.updatedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    }
+}
