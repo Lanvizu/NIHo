6a64b50ce4931e802f432527f325f75f8d2c60b1
Lanvizu <ghzm888@gmail.com>
2024-12-02 17:19:28 +0900

    feat: User 내부 객체 추가 및 빌더 생성

--- src/main/java/com/study/NIHo/domain/user/User.java
+++ src/main/java/com/study/NIHo/domain/user/User.java
@@ -2,23 +2,47 @@ package com.study.NIHo.domain.user;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+import com.study.NIHo.domain.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table(name = "user")
-public class User
-//        implements UserDetails
-{
+@NoArgsConstructor
+public class User extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
+    @Column(unique = true)
+    private String email;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserLevel userLevel;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
+    @Builder
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.userLevel = UserLevel.USER;
+        this.userStatus = UserStatus.NORMAL;
+    }
 }
