bf4957a8c0139153bc2c797b8c8294507a1543f1
Lanvizu <ghzm888@gmail.com>
2024-12-02 17:16:41 +0900

    feat: 예외 처리 메세지 관리하는 UserException 임시 생성

---
+++ src/main/java/com/study/NIHo/util/exception/UserException.java
@@ -0,0 +1,21 @@
+package com.study.NIHo.util.exception;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserException extends RuntimeException {
+
+    private HttpStatus status;
+    private String message;
+
+    public UserException(String message, HttpStatus status) {
+        super(message);
+        this.message = message;
+        this.status = status;
+    }
+}
