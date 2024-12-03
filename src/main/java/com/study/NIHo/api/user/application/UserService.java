8d4bf9bc8769c3a7b452caacd060338e82fb6b78
Lanvizu <ghzm888@gmail.com>
2024-12-03 12:32:20 +0900

    feat: Exception을 Enum으로 처리

--- src/main/java/com/study/NIHo/api/user/application/UserService.java
+++ src/main/java/com/study/NIHo/api/user/application/UserService.java
@@ -1,13 +1,12 @@
-package com.study.NIHo.service;
+package com.study.NIHo.api.user.application;
 
 import com.study.NIHo.domain.user.User;
 import com.study.NIHo.repository.UserRepository;
-import com.study.NIHo.service.dto.user.req.SignUpDto;
-import com.study.NIHo.service.dto.user.res.ResEmailDto;
+import com.study.NIHo.api.user.application.dto.user.request.SignUpDto;
+import com.study.NIHo.api.user.application.dto.user.response.ResponseEmailDto;
 import com.study.NIHo.util.exception.UserException;
-import jakarta.transaction.Transactional;
+import com.study.NIHo.util.exception.UserExceptionResult;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,26 +16,28 @@ public class UserService {
     private final UserRepository userRepository;
 
 //    controller -> service : service Dto
-    public ResEmailDto signUp(SignUpDto dto) {
+    public ResponseEmailDto signUp(SignUpDto dto) {
 
         isExistEmail(dto.getEmail());
         checkSamePassword(dto.getPassword(), dto.getConfirmPassword());
 
         User user = userRepository.save(dto.ofEntity());
 
-        return ResEmailDto.toEmailDto(user);
+        return ResponseEmailDto.toEmailDto(user);
     }
 
     private void isExistEmail(String email) {
         if (userRepository.findByEmail(email).isPresent()) {
 //            Exception Enum file
-            throw new UserException("이미 사용 중인 이메일입니다.", HttpStatus.BAD_REQUEST);
+            throw new UserException(UserExceptionResult.DUPLICATE_EMAIL);
         }
     }
 
     private void checkSamePassword(String password, String confirmPassword) {
         if (!password.equals(confirmPassword)) {
-            throw new UserException("패스워드 불일치", HttpStatus.BAD_REQUEST);
+            throw new UserException(UserExceptionResult.PASSWORD_MISMATCH);
         }
     }
+
+
 }
