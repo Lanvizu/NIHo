e994110409343faefc6c7dd63ad45dc4265f01fa
Lanvizu <ghzm888@gmail.com>
2024-12-03 12:31:43 +0900

    feat: UserLevel -> UserRole로 변경

---
+++ src/main/java/com/study/NIHo/domain/user/UserRole.java
@@ -0,0 +1,23 @@
+package com.study.NIHo.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+    USER("USER"),
+    ADMIN("ADMIN");
+
+    private final String role;
+
+    public static UserRole fromUserRole(String role) {
+        for(UserRole userRole : UserRole.values()) {
+            if(userRole.getRole().equalsIgnoreCase(role)) {
+                return userRole;
+            }
+        }
+
+        throw new IllegalArgumentException("No Role Found!");
+    }
+}
