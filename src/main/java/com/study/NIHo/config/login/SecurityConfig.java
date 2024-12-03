334d110acc941820e7f37bf074019fb9a6c856bf
Lanvizu <ghzm888@gmail.com>
2024-12-03 12:35:33 +0900

    feat: JWT 토큰을 이용한 SecurityConfig 생성

---
+++ src/main/java/com/study/NIHo/config/login/SecurityConfig.java
@@ -0,0 +1,90 @@
+package com.study.NIHo.config.login;
+
+import com.study.NIHo.config.login.security.filter.JwtAuthFilter;
+import com.study.NIHo.config.login.security.handler.CustomAccessDeniedHandler;
+import com.study.NIHo.config.login.security.handler.CustomAuthenticationEntryPointHandler;
+import com.study.NIHo.domain.user.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    private final CustomAuthenticationEntryPointHandler customAuthenticationEntryPointHandler;
+
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+
+    @Bean
+    public SecurityFilterChain config(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector);
+
+        // white list (Spring Security 체크 제외 목록)
+        MvcRequestMatcher[] permitAllWhiteList = {
+                mvc.pattern("/login"),
+                mvc.pattern("/register"),
+                mvc.pattern("/token-refresh"),
+                mvc.pattern("/favicon.ico"),
+                mvc.pattern("/error")
+        };
+
+        // http request 인증 설정
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(permitAllWhiteList).permitAll()
+                // 사용자 삭제는 관리자 권한만 가능
+                .requestMatchers(HttpMethod.DELETE, "/user").hasRole(UserRole.ADMIN.getRole())
+                // 그 외 요청 체크
+                .anyRequest().authenticated()
+        );
+
+        // form login disable
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        // logout disable
+        http.logout(AbstractHttpConfigurer::disable);
+
+        // csrf disable
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // session management
+        http.sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 미사용
+        );
+
+        // before filter
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // exception handler
+        http.exceptionHandling(conf -> conf
+                .authenticationEntryPoint(customAuthenticationEntryPointHandler)
+                .accessDeniedHandler(customAccessDeniedHandler)
+        );
+
+        // build
+        return http.build();
+    }
+}
