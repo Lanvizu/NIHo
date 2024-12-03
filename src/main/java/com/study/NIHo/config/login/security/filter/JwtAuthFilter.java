81e307b2f94a205b7c1517bd0bae727bfc948bb5
Lanvizu <ghzm888@gmail.com>
2024-12-03 12:38:16 +0900

    feat: JWT 토큰 검증과 새로 저장하는 JwtAuthFilter 생성

---
+++ src/main/java/com/study/NIHo/config/login/security/filter/JwtAuthFilter.java
@@ -0,0 +1,62 @@
+package com.study.NIHo.config.login.security.filter;
+
+import com.study.NIHo.api.user.application.UserGetService;
+import com.study.NIHo.api.user.application.dto.user.response.UserGetResponseDTO;
+import com.study.NIHo.config.login.security.provide.JwtProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    private final UserGetService userGetService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        final String token = request.getHeader("Authorization");
+
+        String username = null;
+
+        // Bearer token 검증 후 user name 조회
+        if(token != null && !token.isEmpty()) {
+            String jwtToken = token.substring(7);
+
+            username = jwtProvider.getUsernameFromToken(jwtToken);
+        }
+
+        // token 검증 완료 후 SecurityContextHolder 내 인증 정보가 없는 경우 저장
+        if(username != null && !username.isEmpty() && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Spring Security Context Holder 인증 정보 set
+            SecurityContextHolder.getContext().setAuthentication(getUserAuth(username));
+        }
+
+        filterChain.doFilter(request,response);
+    }
+
+    /**
+     * token의 사용자 idx를 이용하여 사용자 정보 조회하고, UsernamePasswordAuthenticationToken 생성
+     * @param username 사용자 idx
+     * @return 사용자 UsernamePasswordAuthenticationToken
+     */
+    private UsernamePasswordAuthenticationToken getUserAuth(String username) {
+        UserGetResponseDTO user = userGetService.getUserById(Long.parseLong(username));
+
+        return new UsernamePasswordAuthenticationToken(user.userid(), user.password(),
+                Collections.singleton(new SimpleGrantedAuthority(user.userRole().name())));
+    }
+
+}
