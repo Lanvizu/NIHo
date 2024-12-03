85817567c25de44e0b15ea3e8b11521226953d7f
Lanvizu <ghzm888@gmail.com>
2024-12-03 12:42:40 +0900

    feat: ResponseEnum 파일 생성

---
+++ src/main/java/com/study/NIHo/api/common/response/enums/ResponseKeyEnum.java
@@ -0,0 +1,19 @@
+package com.study.NIHo.api.common.response.enums;
+
+import lombok.Getter;
+
+/**
+ * Response Data Key Enum
+ */
+@Getter
+public enum ResponseKeyEnum {
+    LIST("list"),
+    ONE("one"),
+    TOTAL("total");
+
+    private final String key;
+
+    ResponseKeyEnum(String key) {
+        this.key = key;
+    }
+}
