b8f80e03c03a7cf60fcded481cc617edcc7a722b
Lanvizu <ghzm888@gmail.com>
2024-12-03 12:39:14 +0900

    feat: RefreshToken 관련 예외 처리 생성

---
+++ src/main/java/com/study/NIHo/api/token/exception/RefreshTokenException.java
@@ -0,0 +1,15 @@
+package com.study.NIHo.api.token.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Refresh Token Exception
+ */
+@Getter
+@RequiredArgsConstructor
+public class RefreshTokenException extends RuntimeException {
+
+    private final RefreshTokenExceptionResult refreshTokenExceptionResult;
+
+}
