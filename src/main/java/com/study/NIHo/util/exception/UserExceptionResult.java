6bc54b4826a4f02e0020150f1a81b3a4643b1416
Lanvizu <ghzm888@gmail.com>
2024-12-03 12:31:09 +0900

    feat: Enum 타입 UserExceptionResult 생성

---
+++ src/main/java/com/study/NIHo/util/exception/UserExceptionResult.java
@@ -0,0 +1,19 @@
+package com.study.NIHo.util.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserExceptionResult {
+
+    NOT_EXISTS(HttpStatus.BAD_REQUEST, "U0001", "존재하지 않는 사용자입니다."),
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "U0002", "이미 사용 중인 이메일입니다."),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "U0003", "패스워드 불일치");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+}
