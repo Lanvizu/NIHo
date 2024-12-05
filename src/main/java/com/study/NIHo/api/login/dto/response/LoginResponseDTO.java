0edbc5c2884346b67c975046cb806fca086126a1
Lanvizu <ghzm888@gmail.com>
2024-12-05 18:42:48 +0900

    feat: AccessToken을 쿠키로 수정 (JsonIgnore 처리)

--- src/main/java/com/study/NIHo/api/login/dto/response/LoginResponseDTO.java
+++ src/main/java/com/study/NIHo/api/login/dto/response/LoginResponseDTO.java
@@ -1,7 +1,9 @@
 package com.study.NIHo.api.login.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
+import org.springframework.http.ResponseCookie;
 
 @Builder
-public record LoginResponseDTO(String accessToken, String refreshToken) {
+public record LoginResponseDTO(@JsonIgnore ResponseCookie accessTokenCookie, String refreshToken) {
 }
