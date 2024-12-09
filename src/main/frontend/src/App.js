4a29c32ef1c0f11d2ae74ef0a6a765778753bf08
Lanvizu <ghzm888@gmail.com>
2024-12-09 17:04:06 +0900

    feat: (React) 기존 회원가입, 로그인 통합

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
@@ -7,8 +8,7 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/" element={<MainPage />} />
             </Routes>
         </Router>
     );
