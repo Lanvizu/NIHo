52ddbb0a9fc18f63525aa937a7afd1b167d242ed
Lanvizu <ghzm888@gmail.com>
2024-12-10 11:21:32 +0900

    feat: Room 위치 변경 및 수정

--- src/main/java/com/study/NIHo/api/room/domain/entity/Room.java
+++ src/main/java/com/study/NIHo/api/room/domain/entity/Room.java
@@ -1,4 +1,4 @@
-package com.study.NIHo.domain.room;
+package com.study.NIHo.api.room.domain.entity;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -7,11 +7,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@Builder
 @Table(name = "room")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Room {
 
     @Id
