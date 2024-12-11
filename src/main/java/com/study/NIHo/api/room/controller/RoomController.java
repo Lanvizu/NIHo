428a00b30807eee9a671c8d7fd51b719af277c0d
Lanvizu <ghzm888@gmail.com>
2024-12-11 15:30:36 +0900

    feat: RoomController 추가

---
+++ src/main/java/com/study/NIHo/api/room/controller/RoomController.java
@@ -0,0 +1,31 @@
+package com.study.NIHo.api.room.controller;
+
+import com.study.NIHo.api.common.response.entity.ApiResponseEntity;
+import com.study.NIHo.api.room.application.RoomGetService;
+import com.study.NIHo.api.room.dto.response.RoomDetailResponseDTO;
+import com.study.NIHo.api.room.dto.response.RoomListResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/room")
+public class RoomController {
+    private final RoomGetService roomGetService;
+
+    @GetMapping("/list")
+    public ResponseEntity<ApiResponseEntity> getRoomList() {
+        RoomListResponseDTO allRooms = roomGetService.getAllRooms();
+        return ApiResponseEntity.successResponseEntity(allRooms);
+    }
+
+    @GetMapping("/{roomId}")
+    public ResponseEntity<ApiResponseEntity> getRoom(@PathVariable Long roomId) {
+        RoomDetailResponseDTO roomDetail = roomGetService.getRoomDetail(roomId);
+        return ApiResponseEntity.successResponseEntity(roomDetail);
+    }
+}
