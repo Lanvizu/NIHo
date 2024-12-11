40d6e50151b128acc06cc84fe2b82824aea7a837
Lanvizu <ghzm888@gmail.com>
2024-12-11 15:28:35 +0900

    feat: Room 객체 생성

--- src/main/java/com/study/NIHo/api/room/domain/entity/Room.java
+++ src/main/java/com/study/NIHo/api/room/domain/entity/Room.java
@@ -2,8 +2,16 @@ package com.study.NIHo.api.room.domain.entity;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+import com.study.NIHo.api.room.enums.RoomType;
+import com.study.NIHo.api.user.domain.entity.User;
+import com.study.NIHo.api.user.domain.entity.value.LoginInfo;
+import com.study.NIHo.api.user.domain.entity.value.StatusInfo;
+import com.study.NIHo.api.user.dto.request.UserAddRequestDTO;
+import com.study.NIHo.api.user.enums.UserStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -26,8 +34,9 @@ public class Room {
     @Column(name = "room_id")
     private Long id;
 
-    private String type;
-    private int capacity;
-    private int price;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "room_type")
+    private RoomType roomType;
 
+    private int price;
 }
