bcb1ff203035315d44699a8ce407c7a930968714
Lanvizu <ghzm888@gmail.com>
2024-12-12 18:32:38 +0900

    feat: reservation 객체 변경 및 추가

--- src/main/java/com/study/NIHo/api/reservation/domain/entity/Reservation.java
+++ src/main/java/com/study/NIHo/api/reservation/domain/entity/Reservation.java
@@ -1,10 +1,16 @@
 package com.study.NIHo.api.reservation.domain.entity;
 
 import com.study.NIHo.api.common.entity.BaseTimeEntity;
+import com.study.NIHo.api.reservation.domain.entity.value.ReservationDate;
+import com.study.NIHo.api.reservation.dto.request.ReservationAddRequestDTO;
+import com.study.NIHo.api.reservation.enums.ReservationStatus;
 import com.study.NIHo.api.room.domain.entity.Room;
+import com.study.NIHo.api.user.domain.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,7 +18,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,7 +34,12 @@ public class Reservation extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "room_id")
@@ -38,12 +48,16 @@ public class Reservation extends BaseTimeEntity {
     @Embedded
     private ReservationDate reservationDate;
 
-    public Reservation(Room room, LocalDateTime checkIn, LocalDateTime checkOut) {
-        this.room = room;
-        this.reservationDate = new ReservationDate(checkIn, checkOut);
-    }
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReservationStatus status;
 
-    public void updateReservationDate(LocalDateTime checkIn, LocalDateTime checkOut) {
-        this.reservationDate = new ReservationDate(checkIn, checkOut);
+    public static Reservation of(ReservationAddRequestDTO dto, User user, Room room) {
+        return Reservation.builder()
+                .user(user)
+                .room(room)
+                .reservationDate(new ReservationDate(dto.getCheckInDate(), dto.getCheckOutDate()))
+                .status(ReservationStatus.RESERVED)
+                .build();
     }
 }
